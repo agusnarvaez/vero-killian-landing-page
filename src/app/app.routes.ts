e531c8a1aa9f1e1f1094b9f118f8470dd35e20a7
agusnarvaez <agus.narvaez@outlook.com>
2024-01-30 15:00:09 -0300

    Actualizo rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,17 +4,12 @@ import { ProductsComponent } from './pages/products/products.component'
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component'
 import { ContactComponent } from './pages/contact/contact.component'
 import { NotFoundComponent } from './pages/not-found/not-found.component'
-import { HowWeWorkComponent } from './pages/how-we-work/how-we-work.component'
 import { SellComponent } from './pages/sell/sell.component'
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent
-  },
-  {
-    path: 'como-trabajamos',
-    component: HowWeWorkComponent
   },
   {
     path: 'vender',
