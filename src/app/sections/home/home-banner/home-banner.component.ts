c18f7e52cc50ef0eb5c2b048f4e0c4f25bac76d5
agusnarvaez <agus.narvaez@outlook.com>
2024-01-31 14:29:19 -0300

    Maqueto banner

--- src/app/sections/home/home-banner/home-banner.component.ts
+++ src/app/sections/home/home-banner/home-banner.component.ts
@@ -1,32 +1,15 @@
-import { ProductService } from './../../../services/product/product.service'
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { ButtonComponent } from '../../../components/button/button.component'
-import { Router } from '@angular/router'
-import { FilterObject } from '../../../services/filters/filters.service'
+import { RouterLink } from '@angular/router'
 
 @Component({
   selector: 'app-home-banner',
   standalone: true,
-  imports: [CommonModule,ButtonComponent],
+  imports: [CommonModule,RouterLink],
   templateUrl: './home-banner.component.html',
   styleUrl: './home-banner.component.css'
 })
 export class HomeBannerComponent {
-  constructor(private router: Router, private productService: ProductService) {}
-  isRentFilterActive = false
-  isSellFilterActive = false
+  constructor() {}
 
-  isFilterActive = (filterText: string): boolean => {
-    return this.productService.filtersService.isActive("operation_type",filterText)
-  }
-
-  toggleFilter = (filterText: FilterObject): void => this.productService.filtersService.toggle(filterText)
-
-  searchProducts = () => this.router.navigate(['/catalogo'])
-  ngInInit() {
-    this.isRentFilterActive = this.isFilterActive('Alquiler')
-    this.isSellFilterActive = this.isFilterActive('Venta')
-    
-  }
 }
