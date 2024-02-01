d43603140561c049af085b7e62c27ba0da2775e2
agusnarvaez <agus.narvaez@outlook.com>
2024-02-01 16:13:42 -0300

    Agrego home-reviews

--- src/app/sections/home/home-reviews/home-reviews.component.ts
+++ src/app/sections/home/home-reviews/home-reviews.component.ts
@@ -9,30 +9,69 @@ import { CommonModule } from '@angular/common'
   styleUrl: './home-reviews.component.css'
 })
 export class HomeReviewsComponent {
+  actualIndex = 0
   reviews = [
     {
-      author: "C.R.D.",
-      stars: 5,
-      content: "Paula Dallochio, excelentísima gestión, expeditiva, ordenada, super atenta y siempre bien dispuesta. Muchísimas gracias por ayudar a que todo sea más fácil!!"
+      author: "Myriam Gonzalez",
+      avatar: "assets/reviews/myriam-gonzalez.png",
+      content: "Vero tiene una excelentísima gestión, expeditiva, ordenada, super atenta y siempre bien dispuesta. Muchísimas gracias por ayudar a que todo sea más fácil!!"
     },
     {
       author: "N.H.",
-      stars: 4,
-      content: "Trabajar con Paula ha sido un placer, altamente recomendable."
+      avatar: "assets/reviews/myriam-gonzalez.png",
+      content: "Trabajar con Vero ha sido un placer, altamente recomendable."
     },
     {
       author: "Y.C.",
-      stars: 5,
-      content: "Sin dudas excelentes profesionales! Venden cuando nadie vende en el mercado! Pau Dallochio, mi especial agradecimiento por la atención constante, y el gran esfuerzo que hiciste, sos la 1!!!"
+      avatar: "assets/reviews/myriam-gonzalez.png",
+      content: "Sin dudas excelentes profesionales! Venden cuando nadie vende en el mercado! Vero, mi especial agradecimiento por la atención constante, y el gran esfuerzo que hiciste, sos la 1!!!"
     },
     {
       author: "C.T.",
-      stars: 4.3,
+      avatar: "assets/reviews/myriam-gonzalez.png",
+      content: "Excelentes profesionales! Muy atentos a todos los detalles!"
+    },
+    {
+      author: "Myriam Gonzalez",
+      avatar: "assets/reviews/myriam-gonzalez.png",
+      content: "Vero tiene una excelentísima gestión, expeditiva, ordenada, super atenta y siempre bien dispuesta. Muchísimas gracias por ayudar a que todo sea más fácil!!"
+    },
+    {
+      author: "N.H.",
+      avatar: "assets/reviews/myriam-gonzalez.png",
+      content: "Trabajar con Vero ha sido un placer, altamente recomendable."
+    },
+    {
+      author: "Y.C.",
+      avatar: "assets/reviews/myriam-gonzalez.png",
+      content: "Sin dudas excelentes profesionales! Venden cuando nadie vende en el mercado! Vero, mi especial agradecimiento por la atención constante, y el gran esfuerzo que hiciste, sos la 1!!!"
+    },
+    {
+      author: "C.T.",
+      avatar: "assets/reviews/myriam-gonzalez.png",
       content: "Excelentes profesionales! Muy atentos a todos los detalles!"
     }
   ]
 
   makeArray = (num:number) => new Array(Math.round(num))
 
-  isOdd = (num:number) => num % 2 === 1
+  nextCard(){
+    this.isLastPosition()? this.actualIndex = 0 : this.actualIndex++
+  }
+  previousCard(){
+    this.isFirstPosition()? this.actualIndex = this.reviews.length-1 : this.actualIndex--
+  }
+
+  isFirstPosition = () => this.actualIndex === 0
+
+  isLastPosition = () => this.actualIndex === this.reviews.length-1
+
+  carouselPosition(){
+    if(window.innerWidth >= 530){
+      return `translateX(-${100*(this.actualIndex/(this.reviews.length*1.3))}%)`
+    }
+
+    const percentage = 100*(this.actualIndex/this.reviews.length)
+    return `translateX(-${percentage}%)`
+  }
 }
