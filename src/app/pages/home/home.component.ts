7d639024257ff967ab006f911bbe25287cc7b0a2
agusnarvaez <agus.narvaez@outlook.com>
2024-02-14 18:22:53 -0300

    Corrijo tamaño secciones home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,9 @@ import { CommonModule } from '@angular/common'
 import { HomeBannerComponent } from '../../sections/home/home-banner/home-banner.component'
 import { HomeFaqComponent } from '../../sections/home/home-faq/home-faq.component'
 import { HomeReviewsComponent } from '../../sections/home/home-reviews/home-reviews.component'
-import { HomeContactComponent } from '../../sections/home/home-contact/home-contact.component'
 import { HomeIdentityComponent } from '../../sections/home/home-identity/home-identity.component'
 import { HomeWorkComponent } from '../../sections/home/home-work/home-work.component'
 import { HomeAdvicesComponent } from '../../sections/home/home-advices/home-advices.component'
-
 
 @Component({
   selector: 'app-home',
@@ -17,14 +15,11 @@ import { HomeAdvicesComponent } from '../../sections/home/home-advices/home-advi
     HomeBannerComponent,
     HomeReviewsComponent,
     HomeFaqComponent,
-    HomeContactComponent,
     HomeIdentityComponent,
     HomeAdvicesComponent,
-    HomeWorkComponent
+    HomeWorkComponent,
   ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
