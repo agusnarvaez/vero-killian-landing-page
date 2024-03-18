18d892c8988fc6737f1d36316f76f456d31a9f07
agusnarvaez <agus.narvaez@outlook.com>
2024-03-18 19:53:43 -0300

    Correcciones de editor

--- src/app/services/filters/filters.service.ts
+++ src/app/services/filters/filters.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core'
 
-export interface Filters{
-  [key:string]:string | boolean | number
+export interface Filters {
+  [key: string]: string | boolean | number
 }
 
 export interface FilterObject {
@@ -10,24 +10,22 @@ export interface FilterObject {
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FiltersService {
-  private _filters: Filters = {
-  }
+  private _filters: Filters = {}
 
   get = (): Filters => this._filters
-
 
   getSanityQuery(): string {
     const baseQuery = '*[_type == "property"'
     const filterQuery = Object.keys(this._filters)
-      .map(key => {
+      .map((key) => {
         const value = this._filters[key]
         if (typeof value === 'boolean' && value) {
           return ` && ${key} == true`
         } else if (typeof value === 'string') {
-          if(value=='Venta' || value=='Alquiler'){
+          if (value == 'Venta' || value == 'Alquiler') {
             return ` && operation_type->title == "${value}"`
           }
           return ` && ${key} == "${value}"`
@@ -61,30 +59,35 @@ export class FiltersService {
   }
 
   getTokkoQuery(): string {
-    const base_query= {
-      current_localization_id:0,
-      current_localization_type:"country",
-      price_from:0,
-      price_to:999999999,
-      operation_types:[1,2,3],
-      property_types:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],
-      currency:"ANY",
-      filters:[]
+    const base_query = {
+      current_localization_id: 0,
+      current_localization_type: 'country',
+      price_from: 0,
+      price_to: 999999999,
+      operation_types: [1, 2, 3],
+      property_types: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        21, 22, 23, 24, 25,
+      ],
+      currency: 'ANY',
+      filters: [],
     }
 
-    if(this._filters["operation_type"]=="Venta") base_query.operation_types=[1]
+    if (this._filters['operation_type'] == 'Venta')
+      base_query.operation_types = [1]
 
-    if(this._filters["operation_type"]=="Alquiler") base_query.operation_types=[2,3]
+    if (this._filters['operation_type'] == 'Alquiler')
+      base_query.operation_types = [2, 3]
 
-    if(this._filters["type"]=="Departamento") base_query.property_types=[2]
-    else if(this._filters["type"]=="Casa") base_query.property_types=[3]
+    if (this._filters['type'] == 'Departamento') base_query.property_types = [2]
+    else if (this._filters['type'] == 'Casa') base_query.property_types = [3]
 
-    if(this._filters["minPrice"]) base_query.price_from=  Number(this._filters["minPrice"])
-    if(this._filters["maxPrice"]) base_query.price_to=  Number(this._filters["maxPrice"])
-
+    if (this._filters['minPrice'])
+      base_query.price_from = Number(this._filters['minPrice'])
+    if (this._filters['maxPrice'])
+      base_query.price_to = Number(this._filters['maxPrice'])
 
     return JSON.stringify(base_query)
-
   }
 
   add(filterObj: FilterObject): void {
