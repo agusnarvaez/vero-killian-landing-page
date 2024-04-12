3875b3aeeae0d501c20b437500548e520e1cb3fa
agusnarvaez <agus.narvaez@outlook.com>
2024-04-11 21:37:24 -0300

    Agregl faq a routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,28 +5,32 @@ import { ProductDetailComponent } from './pages/product-detail/product-detail.co
 import { ContactComponent } from './pages/contact/contact.component'
 import { NotFoundComponent } from './pages/not-found/not-found.component'
 import { SellComponent } from './pages/sell/sell.component'
-
+import { HomeFaqComponent } from './sections/home/home-faq/home-faq.component'
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: HomeComponent,
   },
   {
     path: 'vender',
-    component: SellComponent
+    component: SellComponent,
   },
   {
     path: 'catalogo',
-    component: ProductsComponent
+    component: ProductsComponent,
   },
   {
     path: 'catalogo/:id',
-    component: ProductDetailComponent
+    component: ProductDetailComponent,
   },
   {
-    path:"contacto",
-    component: ContactComponent
+    path: 'contacto',
+    component: ContactComponent,
   },
+  {
+    path: 'preguntas-frecuentes',
+    component: HomeFaqComponent,
+  },
+  { path: 'notFound', component: NotFoundComponent },
   { path: '**', redirectTo: 'notFound' },
-  { path: 'notFound', component: NotFoundComponent }
 ]
