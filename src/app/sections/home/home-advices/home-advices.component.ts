1ed3823a276a6fc54b9ad27082caf3e9c364d85a
agusnarvaez <agus.narvaez@outlook.com>
2024-04-16 12:39:36 -0300

    Actualizo texto y estilo de home advices

--- src/app/sections/home/home-advices/home-advices.component.ts
+++ src/app/sections/home/home-advices/home-advices.component.ts
@@ -5,19 +5,15 @@ import { RouterLink } from '@angular/router'
 @Component({
   selector: 'app-home-advices',
   standalone: true,
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home-advices.component.html',
-  styleUrl: './home-advices.component.css'
+  styleUrl: './home-advices.component.css',
 })
 export class HomeAdvicesComponent {
-
   public cardsFlipped = [false, false] // Inicializa todos los estados en false
 
   flipCard(index: number) {
     this.cardsFlipped[index] = !this.cardsFlipped[index] // Voltea el estado de la tarjeta clickeada
-    console.log('flipCard', index,this.cardsFlipped)
   }
-  isFlipped(index: number) {
-    return this.cardsFlipped[index] // Retorna el estado de la tarjeta
-  }
+  isFlipped = (index: number) => this.cardsFlipped[index] // Retorna el estado de la tarjeta
 }
