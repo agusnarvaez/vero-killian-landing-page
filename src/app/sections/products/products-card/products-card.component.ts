e58e928eed6accaae19aa9bc1a367de25a8e470b
agusnarvaez <agus.narvaez@outlook.com>
2026-02-05 19:40:48 -0300

    Refactor product card to use app-lazy-image component for optimized image loading

--- src/app/sections/products/products-card/products-card.component.ts
+++ src/app/sections/products/products-card/products-card.component.ts
@@ -2,12 +2,13 @@ import { Component, Input } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { Product } from '../../../models/product'
 import { RouterLink } from '@angular/router'
+import { LazyImageComponent } from '../../../components/lazy-image/lazy-image.component'
 @Component({
   selector: 'app-products-card',
   standalone: true,
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule, RouterLink, LazyImageComponent],
   templateUrl: './products-card.component.html',
-  styleUrl: './products-card.component.css'
+  styleUrl: './products-card.component.css',
 })
 export class ProductsCardComponent {
   @Input() product?: Product
